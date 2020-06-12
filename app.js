3b02d944f9e38934aae8bb734f60af49b506bdc7
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-13 00:22:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div');
+  const result =  document.querySelector('#result');
+  const displayCurrentPlayer = document.querySelector('current-player');
+  let currentPlayer = 1;
+
+  for (var i = 0, i < squares.length; i++) {
+    
+  }
+
+
+
+
+})
